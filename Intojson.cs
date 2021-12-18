20ba494e19615713baab85e5ce8ea5b4f9b289d9
DESKTOP-SHMNK1V\shita <hirotoshinote@gmail.com>
2021-12-19 03:24:58 +0900

    first commit

---
+++ Intojson.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Intojson
+{
+    [System.Serializable]
+    public class aaa
+    {
+        public int a = 1;
+        public int b = 150;
+        public bbb bclass = new bbb();
+        public int[] intArr = new int[5] { 1, 2, 3, 4, 5 };
+        public bbb[] bArr = new bbb[3];
+        public List<ddd> dlist = new List<ddd>();
+        ddd[] dArr = new ddd[3];
+
+        public void ValueIntoArr()
+        {
+            for (int i = 0; i<3; i++)
+            {
+                bArr[i] = new bbb();
+                dArr[i] = new ddd();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                dlist.Add(dArr[i]);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class bbb
+    {
+        public int c = 30;
+    }
+    [System.Serializable]
+    public class ddd
+    {
+        public int e = 50;
+    }
+}
+
---
+++ SampleClass.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SaveData
+{
+    [System.Serializable]
+    public class SampleMapData
+    {
+        public List<Junction> junctionList = new List<Junction>();
+    }
+
+    [System.Serializable]
+    public class Junction
+    {
+        public List<int> holdingCellNum = new List<int>();
+        public List<Cell> holdingCellArr = new List<Cell>();
+        public List<int> nextJunctionArr = new List<int>();
+        Cell[] holdedCell = new Cell[136];
+        
+        public Junction(List<int> holdingCellNum, List<int> nextJunctionArr)
+        {
+            this.holdingCellNum = holdingCellNum;
+            this.nextJunctionArr = nextJunctionArr;
+
+            for(int i = 0; i< holdingCellNum.Count; i++)
+            holdingCellArr.Add(Cell);
+        }
+    }
+
+    [System.Serializable]
+    public class Cell
+    {
+        public int[] cordinates = new int[2];
+        public int effectNum;
+        public int nextCellNum;
+
+        public Cell(int PositionNum ,int effectNum)
+        {
+            int RowNum;
+            int ColumnNum;
+            RowNum = (PositionNum % 8) + 1;
+            ColumnNum = ((PositionNum - (RowNum - 1)) / 8) + 1;
+            cordinates[0] = RowNum;
+            cordinates[1] = ColumnNum;
+            this.effectNum = effectNum;
+        }
+    }
+}
+
