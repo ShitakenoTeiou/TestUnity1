6774596e7121bc76b84e8919fb1cf6be9bb105cf
DESKTOP-SHMNK1V\shita <hirotoshinote@gmail.com>
2022-01-05 23:55:28 +0900

    適当に改変。やりなおし

--- JsonActivater.cs
+++ JsonActivater.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class JsonActivater : MonoBehaviour
 {
+    const int MapRow = 8;
+    const int MapColumn = 17;
+
     public SaveData.SampleMapData SMD = new SaveData.SampleMapData();
     public int[] celleffectNumArr = new int[136]
                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -14,10 +17,11 @@ public class JsonActivater : MonoBehaviour
                                 0, 0,32, 0,40, 0, 0, 0,40, 0, 0,25, 0, 0, 0, 0, 0,
                                 0, 0,10, 0,10, 0, 0, 0,25, 0, 0,40, 0, 0, 0, 0, 0,
                                50,10,10,33,10,40,10,25,10,10,33,10, 0, 0, 0, 0, 0};
+
     public SaveData.Junction[] junctionArr = new SaveData.Junction[7];
     public List<int>[] junctionNextjunction = new List<int>[100];
-    public List<int>[] junctionHoldingCell = new List<int>[100];
-    public List<int>[] junctionHoldingCellCharacter = new List<int>[100];
+    public int[] aCell = new int[2];
+    public List<int[]> CellCordinate = new List<int[]>();
     public int junctionCount;
 
 
@@ -30,8 +34,6 @@ public class JsonActivater : MonoBehaviour
         }
 
         Debug.Log(JsonUtility.ToJson(SMD));
-
-
     }
 
     public void SetMapData()
@@ -46,28 +48,16 @@ public class JsonActivater : MonoBehaviour
         junctionNextjunction[5] = new List<int>() { 6 };
         junctionNextjunction[6] = new List<int>() { 0 };
 
-        junctionHoldingCell[0] = new List<int>() { 119, 120, 121 };
-        junctionHoldingCell[1] = new List<int>() { 104, 87, 70, 53, 36, 37, 38, 39, 40, 57 };
-        junctionHoldingCell[2] = new List<int>() { 122, 123 };
-        junctionHoldingCell[3] = new List<int>() { 106, 89, 72, 73 };
-        junctionHoldingCell[4] = new List<int>() { 74, 75, 76, 93, 110 };
-        junctionHoldingCell[5] = new List<int>() { 123, 124, 125 };
-        junctionHoldingCell[6] = new List<int>() { 127, 128, 129, 130, 113, 96, 79, 62, 45, 44, 43, 42 };
-
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < MapRow; i++)
         {
-            junctionHoldingCellCharacter[i] = new List<int>();
-            for (int j = 0; j < junctionHoldingCell[i].Count; j++)
+            for (int j = 0; j < MapColumn; j++)
             {
-                junctionHoldingCellCharacter[i].Add(celleffectNumArr[junctionHoldingCell[i][j]]);
+                int[] aCell = new int[2] {i, j};
+                CellCordinate.Add(aCell);
             }
-            
         }
 
-        for (int i = 0; i < 7; i++)
-        {
-            junctionArr[i] = new SaveData.Junction(junctionHoldingCell[i], junctionNextjunction[i], junctionHoldingCellCharacter[i]);
-        }
+        
     }
     //void Start()
     //{
--- SampleClass.cs
+++ SampleClass.cs
@@ -12,23 +12,23 @@ namespace SaveData
     [System.Serializable]
     public class Junction
     {
-        public List<int> holdingCellNum = new List<int>();
+        //public List<int> holdingCellNum = new List<int>();
         public List<Cell> holdingCellArr = new List<Cell>();
         public List<int> nextJunctionArr = new List<int>();
         Cell[] holdedCell = new Cell[136];
         
-        public Junction(List<int> holdingCellNum, List<int> nextJunctionArr,List<int> cellCharacterArr)
-        {
-            this.holdingCellNum = holdingCellNum;
-            this.nextJunctionArr = nextJunctionArr;
+        //public Junction(List<int> holdingCellNum, List<int> nextJunctionArr,List<int> cellCharacterArr)
+        //{
+        //    this.holdingCellNum = holdingCellNum;
+        //    this.nextJunctionArr = nextJunctionArr;
 
-            for (int i = 0; i < holdingCellNum.Count; i++)
-            {
-                holdedCell[i] = new Cell(holdingCellNum[i], cellCharacterArr[i]);
-                holdingCellArr.Add(holdedCell[i]);
-            }
+        //    for (int i = 0; i < holdingCellNum.Count; i++)
+        //    {
+        //        holdedCell[i] = new Cell(holdingCellNum[i], cellCharacterArr[i]);
+        //        holdingCellArr.Add(holdedCell[i]);
+        //    }
                 
-        }
+        //}
     }
 
     [System.Serializable]
@@ -38,16 +38,16 @@ namespace SaveData
         public int effectNum;
         public int nextCellNum;
 
-        public Cell(int PositionNum ,int effectNum)
-        {
-            int RowNum;
-            int ColumnNum;
-            RowNum = (PositionNum % 8) + 1;
-            ColumnNum = (PositionNum % 17) + 1;
-            cordinates[0] = RowNum;
-            cordinates[1] = ColumnNum;
-            this.effectNum = effectNum;
-        }
+        //public Cell(int PositionNum ,int effectNum)
+        //{
+        //    int RowNum;
+        //    int ColumnNum;
+        //    RowNum = (PositionNum % 8) + 1;
+        //    ColumnNum = (PositionNum % 17) + 1;
+        //    cordinates[0] = RowNum;
+        //    cordinates[1] = ColumnNum;
+        //    this.effectNum = effectNum;
+        //}
     }
 }
 
